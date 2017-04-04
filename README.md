736951526eede3c41c49a484866f50a9d283616f
johnhigginsavila <johnhiggins.avila@gmail.com>
2017-04-04 19:49:54 +0800

    2017.4.4

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # facebook-like-web-app
+
+This web application is dummy facebook website.
+It is intended for educational purposes only.
+
+Features:
+-login and sign-up
+-add, delete, confirm friends
+-post and news feed
--- server/database/database.js
+++ server/database/database.js
@@ -1,5 +1,5 @@
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize('facebook_dummy_db', 'username', 'password',{
+var sequelize = new Sequelize('facebook_dummy_db', 'postgres', '1234',{
   host:'127.0.0.1',
   dialect:'postgres'
 });
